c08319e98d4c0b502eafe0af2730a0d85fa70d6c
futami <futamiyarn@gmail.com>
2024-11-10 22:28:59 +0700

    fix issue can't build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://astro.build/config
 export default defineConfig({
 	vite: {
 		css: {
 			preprocessorOptions: { scss: { api: 'modern' } }
-		}
+		},
+		plugins: [nodePolyfills()]
 	},
 	output: 'server',
 	adapter: cloudflare(),
