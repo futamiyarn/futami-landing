4198ef35586bbb2f5bf64a0f75d6b2ff64cff9b3
futami <futamiyarn@gmail.com>
2024-11-12 14:23:33 +0700

    Fix image not load

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-	adapter: cloudflare({ imageService: 'cloudflare' }),
+	adapter: cloudflare(),
 	integrations: [tailwind(), svelte()],
 	output: 'server',
 
